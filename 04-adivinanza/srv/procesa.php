4ddc6a4c71e82caaa6cbe2c57e6aff0d65ef5d87
MaximoCrz056 <maximogomezcruz@gmail.com>
2024-10-24 18:58:44 -0600

    mas xd

--- 04-adivinanza/srv/procesa.php
+++ 04-adivinanza/srv/procesa.php
@@ -7,7 +7,7 @@ $respuesta = recuperaTexto("respuesta");
 
 $respuesta = strtolower($respuesta);
 
-if ($respuesta == "hot dog" || $respuesta == "hot-dog" || $respuesta == "hotdog" || $respuesta == "jocho" || $respuesta == "perro caliente" || $respuesta == "perro-caliente" || $respuesta == "mi compa" || $respuesta == "mi-compa") {
+if ($respuesta == "hot dog" || $respuesta == "hot-dog" || $respuesta == "hotdog" || $respuesta == "jocho" || $respuesta == "perro caliente" || $respuesta == "perro-caliente" || $respuesta == "mi compa" || $respuesta == "mi-compa" || $respuesta == "micompa") {
     $resultado = "Correcto la respuesta es Hot Dog";
 } else {
     $resultado = "Incorrecto la respuesta es Hot Dog";
